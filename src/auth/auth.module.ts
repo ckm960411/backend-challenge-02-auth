ccaf81a6531fc1026b73c9532801cc957e5b3d69
ckm960411 <a01098956117@gmail.com>
2025-02-04 21:59:31 +0900

    ✨ Feat: AuthModule 에 JwtModule 및 UserModule 연결

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [JwtModule.register({}), UserModule],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
