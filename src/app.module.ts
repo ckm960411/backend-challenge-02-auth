910cba9df0510f827dae2e13b2f058764dd6fbc4
ckm960411 <a01098956117@gmail.com>
2025-02-04 21:43:12 +0900

    🔧 Config: AppModule 세팅 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,31 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService) => ({
+        type: configService.get('DB_TYPE'),
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: [__dirname + '/**/*.entity.{js,ts}'],
+        synchronize: true,
+      }),
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
