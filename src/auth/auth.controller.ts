4d84de3a3d6fe0402e726873695093ff0ed03a2c
ckm960411 <a01098956117@gmail.com>
2025-02-04 22:36:37 +0900

    ✨ Feat: 회원가입 엔드포인트 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Post } from '@nestjs/common';
+import { User } from 'src/entities/user.entity';
+import { AuthService } from './auth.service';
+import { SignupRequest } from './dto/request/signup.request';
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async signup(
+    @Body() request: SignupRequest,
+  ): Promise<Omit<User, 'password'>> {
+    return this.authService.signUp({
+      email: request.email,
+      name: request.name,
+      password: request.password,
+    });
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/entities/user.entity';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async signUp({
+    name,
+    email,
+    password,
+  }: {
+    name: string;
+    email: string;
+    password: string;
+  }): Promise<Omit<User, 'password'>> {
+    const SALT_ROUNDS = +this.configService.get('SALT_ROUNDS');
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
+    const user = await this.userService.createUser({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+}
