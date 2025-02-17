86f55a4ce9f83c4450d4e88f0b5fe47f7d4f401d
ckm960411 <a01098956117@gmail.com>
2025-02-17 22:12:10 +0900

    모듈 내 의존성 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,17 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from 'src/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtGoogleStrategy } from './strategies/jwt-social-google.strategy';
+import { GoogleAuthGuard } from './strategies/google-auth.guard';
 
 @Module({
-  imports: [JwtModule.register({}), UserModule],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({}),
+    UserModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtGoogleStrategy, GoogleAuthGuard],
 })
 export class AuthModule {}
