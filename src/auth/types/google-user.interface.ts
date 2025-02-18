68ccf8e8e78a404a376d4f53bab94ff449b8c389
ckm960411 <a01098956117@gmail.com>
2025-02-18 20:53:31 +0900

    GoogleUser 타입 수정

--- src/auth/types/google-user.interface.ts
+++ src/auth/types/google-user.interface.ts
@@ -1,10 +1,7 @@
 export interface GoogleUser {
   id: string;
   email: string;
-  verified_email: boolean;
-  name: string;
-  given_name?: string;
-  family_name?: string;
-  picture?: string;
-  locale?: string;
+  firstName: string;
+  lastName: string;
+  photo: string;
 }
