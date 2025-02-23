f7cf1a973ccf78744491e7fbb0ce7f6826160e02
ckm960411 <a01098956117@gmail.com>
2025-02-23 15:09:30 +0900

    CORS 설정 수정

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://backend-challenge.com',
-      'https://api.backend-challenge.com',
-    ],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-    exposedHeaders: ['Authorization'],
-  });
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:3000',
+  //     'https://backend-challenge.com',
+  //     'https://api.backend-challenge.com',
+  //   ],
+  //   credentials: true,
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  //   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  //   exposedHeaders: ['Authorization'],
+  // });
 
   await app.listen(process.env.PORT ?? 80);
 }
