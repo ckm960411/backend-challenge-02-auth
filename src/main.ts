0b9945f2ac00ca2600510bce1bb05e21ec937c46
ckm960411 <a01098956117@gmail.com>
2025-02-23 15:05:28 +0900

    CORS 설정 수정

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,9 @@ async function bootstrap() {
       'https://api.backend-challenge.com',
     ],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    exposedHeaders: ['Authorization'],
   });
 
   await app.listen(process.env.PORT ?? 80);
