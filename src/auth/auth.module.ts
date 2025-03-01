f4c4754cdefc7341560a0499d1d21f8f466aeefd
ckm960411 <a01098956117@gmail.com>
2025-03-01 17:14:54 +0900

    JwtStrategy 등록

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
@@ -8,12 +8,13 @@ import { JwtGoogleStrategy } from './strategies/jwt-social-google.strategy';
 import { GoogleAuthGuard } from './strategies/google-auth.guard';
 import { JwtKakaoStrategy } from './strategies/jwt-social-kakao.strategy';
 import { KakaoAuthGuard } from './strategies/kakao-auth.guard';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({}),
-    UserModule,
+    forwardRef(() => UserModule),
   ],
   controllers: [AuthController],
   providers: [
@@ -22,6 +23,8 @@ import { KakaoAuthGuard } from './strategies/kakao-auth.guard';
     GoogleAuthGuard,
     JwtKakaoStrategy,
     KakaoAuthGuard,
+    JwtStrategy,
   ],
+  exports: [PassportModule, JwtModule, JwtStrategy],
 })
 export class AuthModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { User } from 'src/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
