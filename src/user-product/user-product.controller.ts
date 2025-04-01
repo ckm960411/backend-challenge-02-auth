e83c7c8cd6fbe808c9a003ba1a571f60bdacd9a5
ckm960411 <a01098956117@gmail.com>
2025-04-01 21:01:12 +0900

    UserProduct 목록 조회 엔드포인트 추가

--- src/user-product/user-product.controller.ts
+++ src/user-product/user-product.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { UserProductService } from './user-product.service';
+import { JwtAuthGuard } from 'src/auth/strategies/jwt-auth.guard';
+import { User } from 'src/auth/decorators/user.decorator';
 
 @Controller('user-product')
-export class UserProductController {}
+export class UserProductController {
+  constructor(private readonly userProductService: UserProductService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/')
+  async findAllUserProducts(@User('id') userId: number) {
+    return this.userProductService.findAllUserProducts(userId);
+  }
+}
--- src/user-product/user-product.service.ts
+++ src/user-product/user-product.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserProduct } from 'src/entities/user-product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserProductService {}
+export class UserProductService {
+  constructor(
+    @InjectRepository(UserProduct)
+    private userProductRepository: Repository<UserProduct>,
+  ) {}
+
+  async findAllUserProducts(userId: number) {
+    return this.userProductRepository.find({
+      where: { userId },
+      relations: {
+        product: true,
+        productOption: true,
+      },
+    });
+  }
+}
